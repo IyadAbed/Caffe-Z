fe642defaea64a1ffd1fadeebe4b756729bf53b8
IyadAbed <iyadrabed@gmail.com>
2023-03-18 22:09:43 +0300

    uncomment-js

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-  // let costumer = prompt("Please enter your name");
+  let costumer = prompt("Please enter your name");
 
-  // let costumer1 =prompt('Please enter your gender (male) or (female)');
+  let costumer1 =prompt('Please enter your gender (male) or (female)');
 
-  // if (costumer1 == 'male') {
-  //   alert(`Welcome Mr ${costumer}`);
-  // }
+  if (costumer1 == 'male') {
+    alert(`Welcome Mr ${costumer}`);
+  }
 
-  // else if (costumer1 == 'female') {
-  //   alert(`Welcome Ms ${costumer}` );
-  // }
-  // else {
-  //   alert('Welcome ' + costumer)
-  // }
+  else if (costumer1 == 'female') {
+    alert(`Welcome Ms ${costumer}` );
+  }
+  else {
+    alert('Welcome ' + costumer)
+  }
 
-  // let typeOfDrink =prompt("Do you prefer your drink hot or cold?");
-  // let typeOfTem =prompt('Please write your drink down')
-  // alert('please wait the drink is being prepered')
+  let typeOfDrink =prompt("Do you prefer your drink hot or cold?");
+  let typeOfTem =prompt('Please write your drink down')
+  alert('please wait the drink is being prepered')
   
-  // if (costumer1 == 'male') {
-  //   console.log('Ok Mr ' + costumer + ' ' + 'your order is ' + typeOfDrink + ' ' + typeOfTem);
-  // }
+  if (costumer1 == 'male') {
+    console.log('Ok Mr ' + costumer + ' ' + 'your order is ' + typeOfDrink + ' ' + typeOfTem);
+  }
 
-  // else if (costumer1 == 'female') {
-  //   console.log('Ok Ms ' + costumer + ' ' + 'your order is ' + typeOfDrink + ' ' + typeOfTem);
-  // }
-  // else {
-  //   console.log('Ok ' + costumer + ' ' + 'your order is ' + typeOfDrink + ' ' + typeOfTem);
-  // };
+  else if (costumer1 == 'female') {
+    console.log('Ok Ms ' + costumer + ' ' + 'your order is ' + typeOfDrink + ' ' + typeOfTem);
+  }
+  else {
+    console.log('Ok ' + costumer + ' ' + 'your order is ' + typeOfDrink + ' ' + typeOfTem);
+  };
 
